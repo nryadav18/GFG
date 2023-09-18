06ed05f5481133c9c0f38b5a9f91554ed6f16b4f
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-09-18 16:04:34 +0530

    Added Solution - GfG to GitHub

---
+++ School/Vowel or Not/vowel-or-not.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string isVowel(char c){
+        // code here 
+        if (c=='a' or c=='e' or c=='i' or c=='o' or c=='u' or c=='A' or c=='E' or c=='I' or c=='O' or c=='U')
+        return "YES";
+        else return "NO";
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        char c;
+        cin >> c;
+        Solution ob;
+        cout << ob.isVowel(c) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
