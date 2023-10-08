1275bad61ef3937a74f76c592dadb881c3c28e5a
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-10-08 09:26:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/First Repeating Element/first-repeating-element.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n) {
+        // code here
+        int maxi = *max_element(arr,arr+n);
+        int cnt[maxi+1]={0};
+        for (int i=0;i<n;i++){
+            cnt[arr[i]]++;
+        }
+        for (int i=0;i<n;i++){
+            if (cnt[arr[i]]>=2){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; ++i) cin >> arr[i];
+        Solution ob;
+        cout << ob.firstRepeated(arr, n) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
