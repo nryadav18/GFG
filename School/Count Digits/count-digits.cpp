f25cac9ba1e84f8647316207bf590d9d33f91306
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-10-10 12:30:52 +0530

    Added Solution - GfG to GitHub

---
+++ School/Count Digits/count-digits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int temp = N;
+        int cnt=0;
+        while (temp!=0){
+            int t = temp%10;
+            if (t!=0 and N%t==0){
+                cnt++;
+            }
+            temp/=10;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
