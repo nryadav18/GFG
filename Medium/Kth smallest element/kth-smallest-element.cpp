ca1f72b0380d732bdb2488ba2c9a3b5b242c8f9e
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-10-18 10:54:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kth smallest element/kth-smallest-element.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        int maxi = *max_element(arr,arr+r+1);
+        vector<int>v(maxi+1,0);
+        for (int i=l;i<=r;i++){
+            v[arr[i]]++;
+        }
+        int cnt=0;
+        for (int i=1;i<=maxi;i++){
+            if (v[i]==1){
+                cnt++;
+            }
+            if (cnt==k){
+                return i;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
