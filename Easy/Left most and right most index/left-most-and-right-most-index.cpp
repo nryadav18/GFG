61c4ecc77460f81778672b1f577871b1c5bf7113
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-10-25 14:13:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Left most and right most index/left-most-and-right-most-index.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    pair<long,long> indexes(vector<long long> v, long long x)
+    {
+        // code here
+        int bin = binary_search(v.begin(),v.end(),x);
+        if (!bin){
+            return {-1,-1};
+        }
+        else{
+            auto low = lower_bound(v.begin(),v.end(),x);
+            long int l = low - v.begin();
+            auto upp = upper_bound(v.begin(),v.end(),x);
+            long int u = upp - v.begin() - 1;
+            return {l,u};
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, k;
+        vector<long long>v;
+        cin>>n;
+        for(long long i=0;i<n;i++)
+        {
+            cin>>k;
+            v.push_back(k);
+        }
+        long long x;
+        cin>>x;
+        Solution obj;
+        pair<long,long> pair = obj.indexes(v, x);
+        
+        if(pair.first==pair.second and pair.first==-1)
+        cout<< -1 <<endl;
+        else
+        cout<<pair.first<<" "<<pair.second<<endl;
+
+    }
+	return 0;
+}
+
+// } Driver Code Ends
