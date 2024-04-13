f726a6617d0caa5a8ec71bc122ed18d22bff7a97
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-04-13 14:39:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Even and Odd/even-and-odd.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    void reArrange(int arr[], int N) {
+        // code here
+        vector<int>odd,even;
+        for (int i = 0 ; i < N ; i ++){
+            if (arr[i] % 2 != 0){
+                odd.push_back(arr[i]);
+            }
+            else{
+                even.push_back(arr[i]);
+            }
+        }
+        int i = 0 , j = 0 , k = 0;
+        while (i<odd.size() and j<even.size()){
+            arr[k++] = even[j++];
+            arr[k++] = odd[i++];
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int check(int arr[], int n)
+{
+    int flag = 1;
+    int c=0, d=0;
+    for(int i=0; i<n; i++)
+    {
+        if(i%2==0)
+        {
+            if(arr[i]%2)
+            {
+                flag = 0;
+                break;
+            }
+            else
+                c++;
+        }
+        else
+        {
+            if(arr[i]%2==0)
+            {
+                flag = 0;
+                break;
+            }
+            else
+                d++;
+        }
+    }
+    if(c!=d)
+        flag = 0;
+        
+    return flag;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int arr[N];
+        for(int i=0; i<N; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        ob.reArrange(arr,N);
+        
+        cout<<check(arr,N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
